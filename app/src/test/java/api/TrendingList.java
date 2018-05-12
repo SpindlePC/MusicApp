810f75e43c01b954671970de42a5674230371ae7
spindle <spindlepc@gmail.com>
2018-05-12 13:29:51 +0200

    Nowe 3

---
+++ app/src/test/java/api/TrendingList.java
@@ -0,0 +1,10 @@
+package api;
+
+/**
+ * Created by spindle on 12.05.2018.
+ */
+
+public class TrendingList {
+    public List<TrendingSingle> trending;
+
+}
---
+++ app/src/test/java/api/TrendingSingle.java
@@ -0,0 +1,20 @@
+package api;
+
+/**
+ * Created by spindle on 12.05.2018.
+ */
+
+public class TrendingSingle {
+
+    public int idAlbum;
+    public int idArtist;
+    public int idTrack;
+    public int idTrend;
+    public String strAlbum;
+    public String strArtist;
+    public String strTrack;
+    public String strTrend;
+
+
+
+}
